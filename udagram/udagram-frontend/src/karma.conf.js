c9a6cca216d92cb91bbb353c8d290c1490ee508b
JC <juanc.vivasm@gmail.com>
2023-05-25 21:58:34 +0000

    adding tests

--- udagram/udagram-frontend/src/karma.conf.js
+++ udagram/udagram-frontend/src/karma.conf.js
@@ -25,13 +25,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadlessNoSandbox'],
+    browsers: ['MyChromeHeadless'],
+    singleRun: false,
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
+      MyChromeHeadless: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox','--disable-setuid-sandbox']
-      }
-    },
-    singleRun: false
+        flags: ['--no-sandbox'],
+      },
+    }
   });
 };
